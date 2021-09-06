f6de74e66f2cc391fbdc55c156cf45b9cc70aafc
Pau <paulina.lozano.g@gmail.com>
2021-09-06 14:18:09 -0500

    Commit 1 - initialization of app

---
+++ format.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+fruits = input("Enter list of fruits: ")
+print(fruits)
---
+++ welcome.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#Function to put format to the list
+
+plates = ["Hamburguesa", "HotDog", "Sandwich"]
+
+#def menu(plates):
+#    plates = ["Hamburguesa\n", "HotDog\n", "Sandwich",]
+#    print(plates)
+#    return
+
+
+print("¡Bienvenido! Este es nuestro menu: \n ")
+print(plates)
+
