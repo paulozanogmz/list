1cac24714edc50eee38907c07d854f800498ba63
Pau <paulina.lozano.g@gmail.com>
2021-09-06 14:25:02 -0500

    Commit 2 - 2nd change

--- welcome.py
+++ welcome.py
@@ -2,13 +2,6 @@
 #Function to put format to the list
 
 plates = ["Hamburguesa", "HotDog", "Sandwich"]
-
-#def menu(plates):
-#    plates = ["Hamburguesa\n", "HotDog\n", "Sandwich",]
-#    print(plates)
-#    return
-
-
 print("¡Bienvenido! Este es nuestro menu: \n ")
 print(plates)
 
