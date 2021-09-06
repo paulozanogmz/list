96387d03379400a1b2bf520af4b118cfb5dc0c1a
Pau <paulina.lozano.g@gmail.com>
2021-09-06 17:51:32 -0500

    Commit 1 - test_hamburguesa set up

---
+++ test_hamburguesa.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+ingredientes = ['Pan', 'Carne', 'Lechuga', 'Ketchup', 'Mostaza']
+#print("Los ingredientes de la Hamburguesa son: ")
+#print(ingredientes[0], ingredientes[1], ingredientes[2], ingredientes[3], ingredientes[4], sep='\n')
+
+def hamburguesa(ingredientes):
+	print("Los ingredientes de la Hamburguesa son: ")
+	print(ingredientes[0], ingredientes[1], ingredientes[2], ingredientes[3], ingredientes[4], sep='\n')
+	i = input("Elige ingredientes (Max 5): ")
+	for i in ingredientes:
+		sin = input("Sin: ").split()
+		return
+	
+	
+	
+print(hamburguesa(ingredientes))
