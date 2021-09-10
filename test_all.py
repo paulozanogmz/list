3ff7926e941b77e5911928501f56254b6edce4e8
Pau <paulina.lozano.g@gmail.com>
2021-09-09 20:07:31 -0500

    Commit 5

--- test_all.py
+++ test_all.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 #Menu
 
 
@@ -10,16 +9,18 @@ plate3 = "Sandwich"
 print("¡Bienvenido! Este es nuestro menu: \n ")
 print(plate1, plate2, plate3, sep='\n')
 
-
-pick = input("Elige platillo: ")
-if pick == "Hamburguesa":
-	from test_hamburguesa.py import hamburguesa
-	print(hamburguesa)
-elif pick == "Sandwich":
-	from test_sandwich.py import sandwich
-	print(sandwich)
-elif pick == "Hotdog":
-	from test_hotdog.py import hotdog
-	print(hotdog)
-else:
-	print("No hay eso")
+try:
+    pick = input("Elige platillo: ")
+    if pick == "Hamburguesa":
+	    from test_hamburguesa.py import hamburguesa
+	    print(hamburguesa)
+    elif pick == "Sandwich":
+	    from test_sandwich.py import sandwich
+	    print(sandwich)
+    elif pick == "Hotdog":
+	    from test_hotdog.py import hotdog
+	    print(hotdog)
+    else:
+	    print("No hay eso")
+except EOFError as e:
+    print(end="")
